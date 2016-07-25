29940fbf1ed9f1aef0fa7d0807f1c8ba6e67304d
hearinjeon <wjsgpfls9856@naver.com>
2016-07-25 18:04:04 +0900

    7.25

--- class/webpage/js/gnb.js
+++ class/webpage/js/gnb.js
@@ -1,3 +1,25 @@
 /**
- * Created by 321-19 on 2016-07-22.
+ * Created by 321-09 on 2016-07-22.
  */
+
+$(function(){
+
+    $('.gnb-depth1-link').on('mouseenter',function(){
+        $('.gnb-depth2').delay(500).stop().fadeIn(1000);
+        $('.gnb-bg').stop().animate({
+            height:274
+        },300);
+    });
+
+});
+
+$(function(){
+
+    $('.gnb-depth1-link').on('mouseleave',function(){
+        $('.gnb-depth2').stop().fadeOut(30);
+        $('.gnb-bg').stop().animate({
+            height:0
+        });
+    });
+
+});
